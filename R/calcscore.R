77b5a989027388cdaa5b796cef0577466fdd9cab
Ed Merkle <merklee@missouri.edu>
2013-10-25 00:00:00 +0000

    version 0.5

--- R/calcscore.R
+++ R/calcscore.R
@@ -2,7 +2,7 @@ calcscore <- function(object, ...){
     UseMethod("calcscore")
 }
 
-calcscore.formula <- function(object, fam="pow", param, data, bounds=NULL, reverse=FALSE, ...){
+calcscore.formula <- function(object, fam="pow", param, data, bounds=NULL, reverse=FALSE, ordered=FALSE, ...){
 
     ## For deprecated scaling argument
     dots <- list(...)
@@ -25,20 +25,33 @@ calcscore.formula <- function(object, fam="pow", param, data, bounds=NULL, rever
     forecast <- mf[,2:ncol(mf)]
     if(missing(param)) param <- c(2, rep(1/max(2,NCOL(forecast)), max(2,NCOL(forecast))))
 
-    do.call("calcscore.default", list(object=forecast, outcome=outcome, fam=fam, param=param, bounds=bounds, reverse=reverse))
+    do.call("calcscore.default", list(object=forecast, outcome=outcome, fam=fam, param=param, bounds=bounds, reverse=reverse, ordered=ordered, ...=...))
 
 }
             
 calcscore.default <-
-function(object, outcome, fam="pow", param=c(2,rep(1/max(2,NCOL(forecast)),max(2,NCOL(forecast)))), bounds=NULL, reverse=FALSE, ...){
+function(object, outcome, fam="pow", param=c(2,rep(1/max(2,NCOL(forecast)),max(2,NCOL(forecast)))), bounds=NULL, reverse=FALSE, ordered=FALSE, ...){
 
-    ## For deprecated scaling argument
     dots <- list(...)
 
+    ## For deprecated scaling argument
     if("scaling" %in% names(dots)){
       if(dots$scaling) bounds <- c(0,1)
     }
-  
+
+    ## For Brier score decompositions
+    if("decomp" %in% names(dots)){
+        decomp <- dots$decomp
+        if("group" %in% names(dots)){
+            group <- dots$group
+        } else {
+            group <- rep(1, NROW(object))
+        }
+    } else {
+        decomp <- FALSE
+        group <- 1
+    }
+
     ## Error checks:
     ## Make sure outcome is a vector, then convert it to numeric
     if(!(NROW(outcome)==1 | NCOL(outcome)==1)) stop("outcome must be a vector, not a matrix.\n")
@@ -65,6 +78,10 @@ function(object, outcome, fam="pow", param=c(2,rep(1/max(2,NCOL(forecast)),max(2
     ## Check beta family params
     if(fam=="beta"){
         if(any(param <= -1)) stop("Beta family parameters must be greater than -1")
+        if(ordered){
+            ordered <- FALSE
+            warning("ordered=TRUE has no impact on beta family scores.")
+        }
     }
     ## If forecast is numeric, add 1 to outcome
     if(NCOL(forecast)==1){
@@ -79,55 +96,77 @@ function(object, outcome, fam="pow", param=c(2,rep(1/max(2,NCOL(forecast)),max(2
     ## Number of alternatives and number of parameters
     nalts <- ifelse(NCOL(forecast) <= 2, 2, NCOL(forecast))
     npars <- length(param)
-    
+
     ## For fam=pow or sph, check to ensure that baseline params sum to 1.
     if(fam %in% c("pow","sph")){
-        if(sum(param[2:npars]) != 1){
-            ## If two alternatives and one baseline, take
-            ## complement
-            if(npars==2){
-                if(NCOL(forecast)==1){
-                    param <- c(param[1], 1-param[2], param[2])
+        ## If length(param)==1, then assume this is a rule without
+        ## a baseline.
+        if(npars > 1){
+            if(sum(param[2:npars]) != 1){
+                ## If two alternatives and one baseline, take
+                ## complement
+                if(npars==2){
+                    if(NCOL(forecast)==1){
+                        param <- c(param[1], 1-param[2], param[2])
+                    } else {
+                        warning("Only one baseline parameter supplied. This parameter is assumed to correspond to alternative associated with the first column of forecasts.\n")
+                        param <- c(param, 1-param[2])
+                    }
                 } else {
-                    warning("Only one baseline parameter supplied. This parameter is assumed to correspond to alternative associated with the first column of forecasts.\n")
-                    param <- c(param, 1-param[2])
+                    if(npars != (nalts+1)) stop("Length of param is incorrect.\n")
+                    warning("Baseline parameters were scaled to sum to 1.")
+                    param[2:npars] <- param[2:npars]/sum(param[2:npars])
                 }
-            } else {
-                if(npars != (nalts+1)) stop("Length of param is incorrect.\n")
-                warning("Baseline parameters were scaled to sum to 1.")
-                param[2:npars] <- param[2:npars]/sum(param[2:npars])
             }
         }
-    }    
+    }
     ## END ERROR CHECKING
 
     ## Create data matrix
     if(NCOL(forecast)==1) forecast <- cbind(1-forecast, forecast)
     datmat <- cbind(forecast, outcome)
     ## Obtain unscaled scores
-    sc <- scoreitems(param, datmat, fam)
+    sc <- scoreitems(param, datmat, fam, ordered, decomp, group)
+
+    ## If decomp=TRUE, sc is a list:
+    if(decomp){
+        sclist <- sc
+        sc <- sc$rawscores
+    }
 
     ## Scale if desired
-    if(!is.null(bounds) | reverse){
+    if(!is.null(bounds)){
         ## Aug 26 2013: scoreitems appears to handle multiple alts
         ## 2-alternative examples yield same results as before
-        scalefactor <- scalescores(param, fam)
+        scalefactor <- scalescores(param, fam, ordered, nalts)
 
         lbound <- ifelse(is.na(bounds[1]), 0, bounds[1])
-        ubound <- ifelse(is.na(bounds[2]), 1, bounds[2])
+        ubound <- ifelse(is.na(bounds[2]), 1 + lbound, bounds[2])
 
-        ## Note: reverse is logical but used as 0/1 below.
         if(fam=="beta"){
-            ## TODO consider other scaling for beta family?
             sc <- sc/scalefactor
         } else {
             sc <- (sc - scalefactor[1])/diff(scalefactor)
         }
-        if(reverse) sc <- 1 - sc
         sc <- lbound + (ubound - lbound)*sc
     }
+
+    if(reverse){
+        if(is.null(bounds)){
+            sc <- -sc
+        } else {
+            sc <- lbound + ubound - sc
+        }
+    }
+
     if(any(is.na(sc))){
         stop("Problem with score calculation.  Ensure parameter values are valid.")
     }
+
+    if(decomp){
+        sclist$rawscores <- sc
+        sc <- sclist
+    }
+    
     sc
 }
--- R/fams.R
+++ R/fams.R
@@ -1,5 +1,5 @@
 betafam <-
-function(p, d, param){
+function(p, d, param, ...){
     ## Define integrand:
     ## Inf and NaN arise when 0 is raised to a negative power.
     ## This happens when d==p.
@@ -28,11 +28,17 @@ function(p, d, param){
 
 
 powfam <-
-function(p, d, param){
+function(p, d, param, ...){
     ## Obtain score from power family with baseline for a single
     ## forecast.
     gam <- param[1]
-    cpar <- param[2:length(param)]
+    if(length(param)==1){
+      ## We are using the family without baselines:
+      cpar <- rep(1, length(p))
+    } else {
+      ## We are using the family with baselines
+      cpar <- param[2:length(param)]
+    }
 
     ## JNW 2009, eq (11), numerator of first term
     num1 <- (p[d]/cpar[d])^(gam-1) - 1
@@ -45,11 +51,17 @@ function(p, d, param){
 
 
 sphfam <-
-function(p, d, param){
+function(p, d, param, ...){
     ## Obtain score from pseudospherical family with baseline for a single
     ## forecast.
     gam <- param[1]
-    cpar <- param[2:length(param)]
+    if(length(param)==1){
+      ## We are using the family without baselines:
+      cpar <- rep(1, length(p))
+    } else {
+      ## We are using the family with baselines
+      cpar <- param[2:length(param)]
+    }
 
     ## JNW 2009, eq (12), numerator and denominator of term in square brackets
     num <- p[d]/cpar[d]
@@ -57,4 +69,55 @@ function(p, d, param){
 
     ## Now combine them
     -(1/(gam-1)) * ((num/denom)^(gam-1) - 1)
+}
+
+
+## NB Beta family param is just for a 2-alternative rule,
+##    while pow and sph are for multi-alternatives.
+ordwrap <- function(data, param, fam){
+    ## Wrapper to get ordinal score out of any other family
+    ## (See Jose, Nau, Winkler, 2009, Management Science, Eq (6) + (13))
+    nalts <- ncol(data) - 1
+    p <- data[,1:nalts]
+    p1 <- t(apply(p, 1, cumsum))
+    p1 <- p1[,1:(ncol(p1) - 1)]
+    p2 <- 1 - p1
+    d <- data[,(nalts + 1)]
+    dmat <- matrix(2, nrow(p1), ncol(p1))
+    tmpd <- cbind(d, dmat)
+    dmat <- t(apply(dmat, 1, function(x){
+              x[(x[1]+1):length(x)] <- 1
+              x[2:length(x)]}))
+
+    ## Cumulative baselines for pow and sph
+    if(length(param) > 1){
+        Q <- cumsum(param[2:length(param)])
+    }
+
+    ## FIXME: Modify calcscore to accept different param values
+    ##        for each forecast row, then this loop can be removed.
+    ## TODO: This assumes a single family parameter (ok for pow and sph),
+    ##       followed by baselines.  Greater flexibility will be allowed
+    ##       if baselines are separate argument from family parameters.
+    runscore <- rep(0, nrow(data))
+    for(i in 1:ncol(p1)){
+        if(length(param) > 1){
+            tmpparam <- c(param[1], Q[i], 1 - Q[i])
+        } else {
+            tmpparam <- param
+        }
+        tmpdat <- cbind(p1[,i], p2[,i], dmat[,i])
+
+        tmpscore <- scoreitems(tmpparam, tmpdat, fam = fam,
+                               ordered = FALSE)
+
+        runscore <- runscore + tmpscore
+    }
+
+    runscore
+}
+
+
+logfam <- function(p, d, ...){
+    -log(p[d])
 }
